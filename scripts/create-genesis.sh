da046ae56b2505865a950b1ac6333963152335a9
Gowtham Paramasivam <gowthamparamasivam66@gmail.com>
2022-08-24 10:45:26 +0530

    git ignore and create genesis script udpate

--- scripts/create-genesis.sh
+++ scripts/create-genesis.sh
@@ -4,5 +4,5 @@ echo "===== generating the genesis.block ======"
 BASEDIR=$(dirname "$0")
 # echo "$BASEDIR"
 rm -Rfv $BASEDIR/../artifacts/*
-configtxgen generate --config $BASEDIR/../config-files/crypto-config.yaml --output $BASEDIR/../crypto-config
+configtxgen -configPath $BASEDIR/../config-files/ -outputBlock $BASEDIR/../artifacts/genesis.block -profile MarvelGenesis -channelID system-channel
 echo "Done."
